7e6583836aa8a5784e7f9bcb5fa85c37b0ae7a0b
Ui579826 <69766417+Ui579826@users.noreply.github.com>
2020-08-17 00:16:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const firebaseConfig = {
+
   apiKey: "AIzaSyA71gWgploPltLrRXDQlPTLu4ZWWNBd_FM",
+
   authDomain: "earning-money-625a6.firebaseapp.com",
+
   databaseURL: "https://earning-money-625a6.firebaseio.com",
+
   projectId: "earning-money-625a6",
+
   storageBucket: "earning-money-625a6.appspot.com",
+
   messagingSenderId: "873576076172",
+
   appId: "1:873576076172:web:3ac603301c01eb87e0bca9",
+
   measurementId: "G-69Z8C38SGG"
+
 };
